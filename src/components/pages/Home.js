4d4b5e39532de8c458e898fcdd4f3f5c71ed7ed6
Abhi <Bh00fie@gmail.com>
2023-03-23 23:21:00 +0000

    added socials to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,6 +10,10 @@ function Home() {
       <p> 
         I am currently at my fourth year in my Aerospace and Mechanical Engineering degree at the university of Southampton, I have experience in engineering industry and due to my passion to new technology I am able to transfer skills between my projects, such as logical thinking and a structural method to deal with problems.
       </p>
+      <div className="socials">
+        <a href="https://github.com/Bh00fie"> GitHub</a>
+        <a href="https://www.linkedin.com/in/abhinandan-thour-42359b193/"> LinkedIn</a>
+      </div>
     </div>
   );
 }
