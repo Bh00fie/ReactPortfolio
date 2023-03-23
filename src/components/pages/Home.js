cc0c6c9cd4165d7e698e7144e9db17a6415b8c59
Abhi <Bh00fie@gmail.com>
2023-03-23 23:35:26 +0000

    added button to download cv in the contact me page and an email that the user can use to get in contact with me as well

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,7 +17,7 @@ function Home() {
           <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
         </svg>
           <h2 id="button-text">Download my Resume</h2> 
-          </a>
+      </a>
 
 
       <div className="socials">
