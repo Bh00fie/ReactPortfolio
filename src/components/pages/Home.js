dc5334fef5bfe92a8e8bcf753851b463830e9c3c
Abhi <Bh00fie@gmail.com>
2023-03-23 23:24:34 +0000

    added option to download cv

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,6 +10,16 @@ function Home() {
       <p> 
         I am currently at my fourth year in my Aerospace and Mechanical Engineering degree at the university of Southampton, I have experience in engineering industry and due to my passion to new technology I am able to transfer skills between my projects, such as logical thinking and a structural method to deal with problems.
       </p>
+
+      <a className="btn btn-primary btn-lg" href="images/Abhinandan Thour CV 2023.pdf" download="Abhinandan Thour CV" role="button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
+          <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+          <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+        </svg>
+          <h2 id="button-text">Download my Resume</h2> 
+          </a>
+
+
       <div className="socials">
         <a href="https://github.com/Bh00fie"> GitHub</a>
         <a href="https://www.linkedin.com/in/abhinandan-thour-42359b193/"> LinkedIn</a>
