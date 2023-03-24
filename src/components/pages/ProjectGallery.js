1c76e4c717c389c3bc3d08a8a45c3f61d68d32d2
Abhi <Bh00fie@gmail.com>
2023-03-24 22:55:45 +0000

    added function that will add all necessary information to each card of project

--- src/components/pages/ProjectGallery.js
+++ src/components/pages/ProjectGallery.js
@@ -1,3 +1,23 @@
 import React from 'react';
 import Project from './Project';
 import projects from '../projects.json';
+
+function ProjectGallery() {
+  return (
+    <div>
+      <h1 className="project-title text-center p-5">Projects</h1>
+      <div className="project-container container">
+        <div className="row row-cols-sm-1 row-cols-lg-2 g-4">
+          {projects.map(project => (
+            <Project 
+              key={project.key}
+              {...project}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+  
+export default ProjectGallery;
