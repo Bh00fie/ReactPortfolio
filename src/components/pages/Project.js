3e73a78b9c0fc317c72d3a1ac1af206329e3cd80
Abhi <Bh00fie@gmail.com>
2023-03-24 22:50:10 +0000

    imported images for each function

---
+++ src/components/pages/Project.js
@@ -0,0 +1,12 @@
+// Imported React
+import React from 'react';
+
+// Imported Images for each project
+import workoutKitchen from "../images/workoutKitchen.png";
+import weightTracker from "../images/weightTracker.png";
+import readmeGenerator from "../images/ReadMe.png";
+import codeQuiz from "../images/Quiz.jpg";
+import weatherAPI from "../images/weatherAPI.png";
+import dailyPlanner from "../images/dailyPlanner.png";
+
+
