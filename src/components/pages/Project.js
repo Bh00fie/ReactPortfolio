8eff8d4d3390834f86670d33698dedd9e3165442
Abhi <Bh00fie@gmail.com>
2023-03-24 22:51:12 +0000

    added function to check if each project is deployed or not

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -9,4 +9,24 @@ import codeQuiz from "../images/Quiz.jpg";
 import weatherAPI from "../images/weatherAPI.png";
 import dailyPlanner from "../images/dailyPlanner.png";
 
+const images = {
+  "Workout Kitchen": workoutKitchen,
+  "Team Profile Generator": weightTracker,
+  "README Generator": readmeGenerator,
+  "Code Quiz": codeQuiz,
+  "Password Generator": weatherAPI,
+  "Work Day Scheduler": dailyPlanner,
+};
 
+//Created function to check if the webpage has been deployed or not
+function Project({ title, description, features, deployed, repo }) {
+  const isDeployed = () => {
+    if (deployed) {
+      return <a href={deployed}><button>Deployed App</button></a>;
+    } else {
+      return null;
+    }
+  };
+}
+
+export default Project;
---
+++ src/components/pages/ProjectGallery.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Project from './Project';
+import projects from '../projects.json';
+
+function ProjectGallery() {
+  //sets the 'project' state to all the projects in my json file
+  const [project] = useState(projects)
+    return (
+      <div>
+        <h1 className="project-title text-center p-5">Projects</h1>
+        <div className="project-container container">
+          <div className="row row-cols-sm-1 row-cols-lg-2 g-4">
+            {/* iterates through the projects and displays the info for each one*/}
+            {project.map(project => (
+            <Project 
+            title={project.title}
+            image={project.image}
+            description={project.description}
+            features={project.features}
+            deployed={project.deployed}
+            repo={project.repo}
+            key={project.key}
+            />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+}
+  
+export default ProjectGallery;
